dd856e1c24cb0503f73e27d11634f5e2d64813a6
Glesyne <alexc161@nycstudents.net>
2025-05-05 21:29:01 +0000

    Final Version

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
-
+public class Main{
+    public static void main (String[] args) {
+        WordMatch w = new WordMatch("mississippi"); 
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        System.out.println();
+        WordMatch b = new WordMatch("aaaabb"); 
+        System.out.println(b.scoreGuess("a"));
+        System.out.println(b.scoreGuess("aa"));
+        System.out.println(b.scoreGuess("aaa"));
+        System.out.println(b.scoreGuess("aabb"));
+        System.out.println(b.scoreGuess("c"));
+        System.out.println();
+        WordMatch c = new WordMatch("concatenation"); 
+        System.out.println(c.scoreGuess("ten"));
+        System.out.println(c.scoreGuess("nation"));
+        System.out.println(c.findBetterGuess("ten", "nation"));
+        System.out.println(c.scoreGuess("con"));
+        System.out.println(c.scoreGuess("cat"));
+        System.out.println(c.findBetterGuess("con", "cat"));
+    }
+}
